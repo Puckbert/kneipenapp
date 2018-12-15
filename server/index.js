7fec7f179e97b60d1c1681d1da2d3252836a432e
puckbert <aleeex.bh@gmail.com>
2018-12-15 15:47:03 +0100

    Connection to Mongo

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const kneipenDB = require('monk')('localhost/kneipenapp');
+const morgan = require('morgan');
+const rad2deg = require('./radiusBerechnen');
+
 const PORT = 5000;
 
 const app = express();
+const kneipen = kneipenDB.get('kneipen');
+console.log(rad2deg(3.1415));
+
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
    res.json({
        message: 'WORKS!'
    });
+});
+
+app.get('/alleKneipen', (req, res) => {
+    kneipen.find({}, {standort: 1,_id:0}).then( kneipenListe => {
+        console.log(kneipenListe);
+        res.json(JSON.stringify(kneipenListe));
+    })
 });
 
 app.listen(PORT, () =>{
---
+++ server/radiusBerechnen.js
@@ -0,0 +1,5 @@
+function rad2deg(angle){
+    return angle / Math.PI * 180;
+}
+
+module.exports = rad2deg;
