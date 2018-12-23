c82151903dec42e63287485bd85a9a784b94dae8
puckbert <aleeex.bh@gmail.com>
2018-12-23 13:49:32 +0100

    Erste Route

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const kneipenDB = require('monk')('localhost/kneipenapp');
 const morgan = require('morgan');
+const cors = require('cors');
+const Joi = require('joi');
 
 const PORT = 5000;
 
+const geoSchema = Joi.object().keys({
+    type: Joi.string().default('Point'),
+    coordinates: Joi.array().items(Joi.number())
+});
+
+const kneipeSchema = Joi.object().keys({
+    name: Joi.string().required(),
+    oeffnungszeiten: Joi.array().items(Joi.string()),
+    geometry: geoSchema
+}) ;
+
 const app = express();
 const kneipen = kneipenDB.get('kneipen');
-
+app.use(cors());
 app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
@@ -15,15 +28,26 @@ app.get('/', (req, res) => {
    });
 });
 
-app.get('/alleKneipen', (req, res) => {
-    kneipen.find({}, 'name').then( kneipenListe => {
-        for (i=0;i<kneipenListe.length;i++){
-            console.log(kneipenListe[i].name);
-        }
-        res.json(kneipenListe);
-      
+app.get('/getKneipeVonName', (req, res) => {
+    kneipen.find({ name: req.query.name }).then(kneipe => {
+        res.send(kneipe);
     })
 });
+
+app.get('/getKneipeInUmkreis', (req, res) => {    
+    kneipen.geoNear(
+        {type: 'Point', coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]},
+        {maxDistance: parseFloat(req.query.rad), spherical: true}
+    ).then((kneipenListe) => {
+        res.send(kneipenListe);
+    });
+});
+
+app.get('/alleKneipen', (req, res) => {
+    kneipen.find().then(alleKneipen => {
+        res.send(alleKneipen);
+    })
+})
 
 app.listen(PORT, () =>{
     console.log('Listening On Port: '+PORT);
